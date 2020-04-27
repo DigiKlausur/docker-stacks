beda1417d3d8f285c75ce58f944831bf168cd1ff
mhwasil <mwasil@outlook.co.id>
2020-04-27 16:03:00 +0200

    Add notebook-dev image

---
+++ images/notebook-dev/nbgrader_config.py
@@ -0,0 +1,10 @@
+from nbgrader.exchange import ngshare
+c = get_config()
+c.ExchangeFactory.exchange = ngshare.Exchange
+c.ExchangeFactory.fetch_assignment = ngshare.ExchangeFetchAssignment
+c.ExchangeFactory.fetch_feedback = ngshare.ExchangeFetchFeedback
+c.ExchangeFactory.release_assignment = ngshare.ExchangeReleaseAssignment
+c.ExchangeFactory.release_feedback = ngshare.ExchangeReleaseFeedback
+c.ExchangeFactory.list = ngshare.ExchangeList
+c.ExchangeFactory.submit = ngshare.ExchangeSubmit
+c.ExchangeFactory.collect = ngshare.ExchangeCollect
