0b4779bbe6057a964604241264149adb06ec9df7
Mohammad Wasil <mwasil@outlook.co.id>
2020-11-13 17:15:55 +0100

    add hub_dev and ngshare_dev

--- .travis/build-dev.sh
+++ .travis/build-dev.sh
@@ -5,15 +5,16 @@ VERSION=$(git rev-parse --short HEAD)
 make -C ../minimal-notebook build
 docker run -it --rm -d -p 8880:8888 digiklausur/minimal-notebook:latest
 
-make -C notebook build_dev
+make -C ../notebook build_dev
 docker run -it --rm -d -p 8881:8888 digiklausur/notebook-dev:$VERSION
 
-make -C restricted-notebook build_dev
+make -C ../restricted-notebook build_dev
 docker run -it --rm -d -p 8882:8888 digiklausur/restricted-notebook-dev:$VERSION
 
-make -C ../ngshare build
+make -C ../ngshare build_dev
 docker run -it --rm -d -p 8883:8888 digiklausur/ngshare:$VERSION
 
 make -C ../hub build
 
 docker ps
+docker images
--- .travis/deploy-dev.sh
+++ .travis/deploy-dev.sh
@@ -15,9 +15,12 @@ make -C restricted-notebook build_dev
 make push -C restricted-notebook push_dev
 docker run -it --rm -d -p 8882:8888 digiklausur/restricted-notebook-dev:$VERSION
 
-make -C ../ngshare build
+make -C ../ngshare build_dev
+make -C ../ngshare push_dev
 docker run -it --rm -d -p 8883:8888 digiklausur/ngshare:$VERSION
 
-make -C ../hub build
+make -C ../hub build_dev
+make -C ../hub push_dev
 
 docker ps
+docker images
