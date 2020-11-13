5639ef1c107a2f62587894f829dd5ca5d77afb95
Mohammad Wasil <mwasil@outlook.co.id>
2020-11-13 17:21:43 +0100

    fix wrong path

--- .travis/build-dev.sh
+++ .travis/build-dev.sh
@@ -2,19 +2,19 @@
 
 VERSION=$(git rev-parse --short HEAD)
 
-make -C ../minimal-notebook build
+make -C minimal-notebook build
 docker run -it --rm -d -p 8880:8888 digiklausur/minimal-notebook:latest
 
-make -C ../notebook build_dev
+make -C notebook build_dev
 docker run -it --rm -d -p 8881:8888 digiklausur/notebook-dev:$VERSION
 
-make -C ../restricted-notebook build_dev
+make -C restricted-notebook build_dev
 docker run -it --rm -d -p 8882:8888 digiklausur/restricted-notebook-dev:$VERSION
 
-make -C ../ngshare build_dev
+make -C ngshare build_dev
 docker run -it --rm -d -p 8883:8888 digiklausur/ngshare:$VERSION
 
-make -C ../hub build
+make -C hub build_dev
 
 docker ps
 docker images
--- .travis/deploy-prod.sh
+++ .travis/deploy-prod.sh
@@ -4,24 +4,24 @@ env | grep DOCKER
 echo "$DOCKER_TOKEN" | docker login --username "$DOCKER_USERNAME" --password-stdin
 VERSION=$(git rev-parse --short HEAD)
 
-make -C ../minimal-notebook build
-make push -C ../minimal-notebook push
+make -C minimal-notebook build
+make -C minimal-notebook push
 docker run -it --rm -d -p 8880:8888 digiklausur/minimal-notebook:latest
 
-make -C ../notebook build
-make push -C ../notebook push
+make -C notebook build
+make -C notebook push
 docker run -it --rm -d -p 8881:8888 digiklausur/notebook:$VERSION
 
-make -C ../restricted-notebook build
-make push -C ../restricted-notebook push
+make -C restricted-notebook build
+make -C restricted-notebook push
 docker run -it --rm -d -p 8882:8888 digiklausur/restricted-notebook:$VERSION
 
-make -C ../ngshare build
-make push -C ../ngshare push
+make -C ngshare build
+make -C ngshare push
 docker run -it --rm -d -p 8883:8888 digiklausur/ngshare:$VERSION
 
-make -C ../hub build
-make push -C ../hub push
+make -C hub build
+make -C hub push
 
 docker ps
 docker images
