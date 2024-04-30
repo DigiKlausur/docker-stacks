cb4a9be573a8484d11172ee20a76178afdd08d68
Tim Metzler <33530562+tmetzl@users.noreply.github.com>
2024-04-30 13:39:14 +0200

    Update nbgrader_config.py

--- images/e2xgrader-notebook/configs/nbgrader_config.py
+++ images/e2xgrader-notebook/configs/nbgrader_config.py
@@ -3,3 +3,9 @@ from e2xgrader.config import configure_base, configure_exchange
 c = get_config()  # noqa: F821
 configure_base(c)
 configure_exchange(c)
+
+# Fix permissions for exchange directory
+c.BaseConverter.permissions = 444      # r--r--r--
+c.GenerateAssignment.permissions = 664 # rw-rw-r--
+c.GenerateFeedback.permissions = 664   # rw-rw-r--
+c.GenerateSolution.permissions = 664   # rw-rw-r--
